e79291bef91a50da214326907f254163336c5373
tebracb <bori.temesi@gmail.com>
2019-10-15 11:20:50 +0200

    experimenting with street view panorama instead of static image in sidebar

--- restaurant-review/src/components/SidebarInfo.js
+++ restaurant-review/src/components/SidebarInfo.js
@@ -1,69 +1,109 @@
 import React from "react"
 import Ratings from "react-ratings-declarative";
 import ReactDOM from "react-dom";
+import ReactStreetview from 'react-streetview';
 
 class SidebarInfo extends React.Component {
     constructor(props) {
-        super(props)
+        super(props)  
     }
 
+
+       
     componentDidUpdate() {
         ReactDOM.findDOMNode(this).scrollIntoView();
     }
 
     render() {
+       
 
-    //    let setPanorama = () => {
-        this.lat = typeof this.props.selectedRestaurant.geometry.location.lat === "function" ? this.props.selectedRestaurant.geometry.location.lat() : this.props.selectedRestaurant.geometry.location.lat
-        this.lng = typeof this.props.selectedRestaurant.geometry.location.lng === "function" ? this.props.selectedRestaurant.geometry.location.lng() : this.props.selectedRestaurant.geometry.location.lng
+        if (this.props.selectedRestaurant) {
+            this.position = {
+                 lat:typeof this.props.selectedRestaurant.geometry.location.lat === "function" ? this.props.selectedRestaurant.geometry.location.lat() : this.props.selectedRestaurant.geometry.location.lat,
+                 lng:typeof this.props.selectedRestaurant.geometry.location.lng === "function" ? this.props.selectedRestaurant.geometry.location.lng() : this.props.selectedRestaurant.geometry.location.lng
+            } 
+            console.log(this.props.selectedRestaurant.geometry.location)
+         }
 
-        // let lat = typeof this.props.selectedRestaurant.geometry.location.lat === "function" ? this.props.selectedRestaurant.geometry.location.lat() : this.props.selectedRestaurant.geometry.location.lat
-        // let lng = typeof this.props.selectedRestaurant.geometry.location.lng === "function" ? this.props.selectedRestaurant.geometry.location.lng() : this.props.selectedRestaurant.geometry.location.lng
+         this.streetViewPanoramaOptions = {
+            position: this.position,
+            pov: { heading: 100, pitch: 0 },
+            zoom: 1
+        };
+      
+        // see https://developers.google.com/maps/documentation/javascript
+        const googleMapsApiKey = 'AIzaSyDbdKdJc9wSQ83SQAX9B34xJ_cydDMUQnQ&';
+        // this.lat = typeof this.props.selectedRestaurant.geometry.location.lat === "function" ? this.props.selectedRestaurant.geometry.location.lat() : this.props.selectedRestaurant.geometry.location.lat
+        // this.lng = typeof this.props.selectedRestaurant.geometry.location.lng === "function" ? this.props.selectedRestaurant.geometry.location.lng() : this.props.selectedRestaurant.geometry.location.lng
 
-               
-               
-        //    let panorama = new window.google.maps.StreetViewPanorama(
-             
-        //       {
-        //         position: lat, lng,
-        //         pov: { heading: 165, pitch: 0 },
-        //         zoom: 1
-        //       })
-        //     }
 
-        return (
-            <div className="restaurantInfoDiv">
-                <div className="restaurantName">
-                    <button className="closeButton" onClick={this.props.handleCloseClick}>X</button>
-                    <h2>{this.props.selectedRestaurant.name}</h2>
+        // see https://developers.google.com/maps/documentation/javascript/3.exp/reference#StreetViewPanoramaOptions
+        
+        
+        
+    
 
-                    <Ratings
-                        rating={this.props.selectedRestaurant.rating}
-                        widgetDimensions="20px"
-                        widgetSpacings="2px"
-                        widgetRatedColors="#FFD300"
-                    >
-                        <Ratings.Widget />
-                        <Ratings.Widget />
-                        <Ratings.Widget />
-                        <Ratings.Widget />
-                        <Ratings.Widget />
-                    </Ratings>
-                </div>
 
-                <img alt="Google Street View" src={`https://maps.googleapis.com/maps/api/streetview?size=300x200&location=${this.lat},${this.lng}&fov=90&heading=235&pitch=10&key=AIzaSyDbdKdJc9wSQ83SQAX9B34xJ_cydDMUQnQ&`} />
-               
-                   {/* {panorama} */}
-                
-                <p><img alt="location icon" className="addressIcon" src={require("./img/location-pin.png")} />{this.props.selectedRestaurant.formatted_address}</p>
-                {this.props.selectedRestaurant.place_id && this.props.restaurantDetails ? 
-                <div> 
+
+
+//    let setPanorama = () => {
+
+// let lat = typeof this.props.selectedRestaurant.geometry.location.lat === "function" ? this.props.selectedRestaurant.geometry.location.lat() : this.props.selectedRestaurant.geometry.location.lat
+// let lng = typeof this.props.selectedRestaurant.geometry.location.lng === "function" ? this.props.selectedRestaurant.geometry.location.lng() : this.props.selectedRestaurant.geometry.location.lng
+
+//    let panorama = new window.google.maps.StreetViewPanorama(
+
+//       {
+//         position: lat, lng,
+//         pov: { heading: 165, pitch: 0 },
+//         zoom: 1
+//       })
+//     }
+
+return (
+    <div className="restaurantInfoDiv">
+        <div className="restaurantName">
+            <button className="closeButton" onClick={this.props.handleCloseClick}>X</button>
+            <h2>{this.props.selectedRestaurant.name}</h2>
+
+            <Ratings
+                rating={this.props.selectedRestaurant.rating}
+                widgetDimensions="20px"
+                widgetSpacings="2px"
+                widgetRatedColors="#FFD300"
+            >
+                <Ratings.Widget />
+                <Ratings.Widget />
+                <Ratings.Widget />
+                <Ratings.Widget />
+                <Ratings.Widget />
+            </Ratings>
+        </div>
+
+        {/* <img alt="Google Street View" src={`https://maps.googleapis.com/maps/api/streetview?size=300x200&location=${this.lat},${this.lng}&fov=90&heading=235&pitch=10&key=AIzaSyDbdKdJc9wSQ83SQAX9B34xJ_cydDMUQnQ&`} /> */}
+
+        {/* {panorama} */}
+        <div style={{
+            width: '800px',
+            height: '450px',
+            backgroundColor: '#eeeeee'
+        }}>
+            <ReactStreetview
+                apiKey={googleMapsApiKey}
+                streetViewPanoramaOptions={this.streetViewPanoramaOptions}
+            />
+        </div>
+
+
+        <p><img alt="location icon" className="addressIcon" src={require("./img/location-pin.png")} />{this.props.selectedRestaurant.formatted_address}</p>
+        {this.props.selectedRestaurant.place_id && this.props.restaurantDetails ?
+            <div>
                 <p><img alt="phone icon" className="addressIcon" src={require("./img/call1.png")} />{this.props.restaurantDetails.formatted_phone_number}</p>
                 <p><img alt="website icon" className="addressIcon siteIcon" src={require("./img/www.png")} /><span><a href={this.props.restaurantDetails.website}>{this.props.restaurantDetails.website}</a></span></p>
-                </div>
-                 : null}
             </div>
-        )
+            : null}
+    </div>
+)
     }
 }
 
