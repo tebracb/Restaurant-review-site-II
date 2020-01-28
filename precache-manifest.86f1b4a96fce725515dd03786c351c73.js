1bcf739a8a92c753d4eef180afb309913f9506e9
tebracb <bori.temesi@gmail.com>
2020-01-28 20:42:59 +0100

    Updates

---
+++ precache-manifest.86f1b4a96fce725515dd03786c351c73.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bdd1cb85e533a252a65d4b200a99a9f",
+    "url": "/Restaurant-review-site-II/index.html"
+  },
+  {
+    "revision": "24ebd5b7b9786f914010",
+    "url": "/Restaurant-review-site-II/static/css/main.f7202843.chunk.css"
+  },
+  {
+    "revision": "358d08a1e4a38cf3338c",
+    "url": "/Restaurant-review-site-II/static/js/2.5edd40ff.chunk.js"
+  },
+  {
+    "revision": "24ebd5b7b9786f914010",
+    "url": "/Restaurant-review-site-II/static/js/main.5d61925a.chunk.js"
+  },
+  {
+    "revision": "273d90636ec99de6af78",
+    "url": "/Restaurant-review-site-II/static/js/runtime~main.d4256ea2.js"
+  },
+  {
+    "revision": "ecce88325b8a84ab69e8a7185c7c4c02",
+    "url": "/Restaurant-review-site-II/static/media/call1.ecce8832.png"
+  },
+  {
+    "revision": "bb103874cfe6d9b37fede46631c536a9",
+    "url": "/Restaurant-review-site-II/static/media/lasagna.bb103874.png"
+  },
+  {
+    "revision": "e895bc9d934712512cf8106cf6b02a36",
+    "url": "/Restaurant-review-site-II/static/media/location-pin.e895bc9d.png"
+  },
+  {
+    "revision": "7f69b99b4107a48e79bc7990b76bf992",
+    "url": "/Restaurant-review-site-II/static/media/restaurant-placeholder.7f69b99b.jpg"
+  },
+  {
+    "revision": "d27e536c0131d7c97f43e50d8c686ba4",
+    "url": "/Restaurant-review-site-II/static/media/www.d27e536c.png"
+  }
+]);
